893202c22969fc96ff2fb3f374ccdfabb705dbbe
sardorbek3112 <raupovsardorbek.3107@gmail.com>
2022-07-22 23:09:35 +0500

    if06.py

--- if06.py
+++ if06.py
@@ -1,3 +1,25 @@
+def mix(a,b,c):
+    if a>b:
+        if a>c:
+            return a
+        else:
+            return c
+    else:
+        if b>c:
+            return b
+        else:
+            return c
+def max(a,b,c,n,m,o):
+    if a>b:
+        if a>c:
+            return n
+        else:
+            return o
+    else:
+        if b>c:
+            return m
+        else:
+            return o
 def main(n):
     """
     Find the index of the largest digit of the five-digit number.
@@ -6,4 +28,13 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    x1 = n%10
+    x2 = n//10%10
+    x3 = n//100%10
+    x4 = n//1000%10
+    x5 = n//10000%10
+    mx = mix(x1,x2,x3)
+    mi = max(x1,x2,x3,1,2,3)
+    mi = max(mx,x4,x5,mi,4,5) 
+    return mi
+

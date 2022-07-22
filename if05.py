9ad28e12df2c768c4617db5874e0fb249c6f04dc
sardorbek3112 <raupovsardorbek.3107@gmail.com>
2022-07-22 22:11:58 +0500

    if05.py

--- if05.py
+++ if05.py
@@ -1,3 +1,6 @@
+def max(a,b):
+    if a>b:return a
+    return b
 def main(n):
     """
     Find the largest digit of the five-digit number.
@@ -6,4 +9,14 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    x1 = n%10
+    x2 = n//10%10
+    x3 = n//100%10
+    x4 = n//1000%10
+    x5 = n//10000%10
+    mx = max(x1,x2)
+    mx = max(mx,x3)
+    mx = max(mx,x4)
+    mx = max(mx,x5)
+    return mx
+    
